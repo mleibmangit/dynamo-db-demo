eae67a3ec1ac4e2bf46174f6990a6736c401e1f0
mleibmangit <mleibman@gmail.com>
2021-12-19 17:45:17 +0200

    initial commit

---
+++ src/test/java/com/michael/dynamodbdemo/DynamoDbDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michael.dynamodbdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamoDbDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
