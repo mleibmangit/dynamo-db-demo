2521438885f61365060c9ce2f15de895c6da13a7
mleibmangit <mleibman@gmail.com>
2022-01-06 22:09:42 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# dynamodbdemo
+# dynamodb demo
+
+1) Requirements
+2) Data modeling: single table, streams, indexes, transactions, optimistic locking, TTL
+3) Code
+4) Deployment, docker/docker compose
+
+Smart flight ordering system:
+
+Rest api:
+
+1. Create a airplane. - Method:POST - Endpoint: /airplane - Payload:
+2. Create a flight. - Method:POST - Endpoint: /flight - Payload:
+3. Create a customer.- Method:POST - Endpoint: /customer - Payload:
+4. Find a most suitable flight by date/price/...,
+5. Make an order. (Buy ticket)
+
---
+++ src/main/java/com/michael/dynamodbdemo/config/DynamoDbConfig.java
@@ -0,0 +1,34 @@
+package com.michael.dynamodbdemo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+
+import java.net.URI;
+
+@Configuration
+public class DynamoDbConfig {
+
+    @Bean
+    public DynamoDbClient getDynamoDbClient() {
+        AwsCredentialsProvider credentialsProvider =
+                DefaultCredentialsProvider.builder()
+                        .build();
+
+        return DynamoDbClient.builder()
+                .region(Region.US_EAST_1)
+                .endpointOverride(URI.create("http://localhost:8000"))
+                .credentialsProvider(credentialsProvider).build();
+    }
+
+    @Bean
+    public DynamoDbEnhancedClient getDynamoDbEnhancedClient() {
+        return DynamoDbEnhancedClient.builder()
+                .dynamoDbClient(getDynamoDbClient())
+                .build();
+    }
+}
